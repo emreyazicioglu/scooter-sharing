ad2de035edef4786aebfa87966d4abccc7d6bfec
Emre Yazicioglu <emreyaziciogl@gmail.com>
2024-07-18 17:43:55 +0300

    Popups are fixed completely

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import scooters from "../data/ankara-scooters.json";
 import { useNavigate } from "react-router-dom";
@@ -24,67 +24,58 @@ const MapboxExample = () => {
     console.log("Selected scooter state:", scooter);
   };
 
-  const pins = () =>
-    scooters.map((scooter) => (
-      <Marker
-        key={scooter.id}
-        longitude={scooter.longitude}
-        latitude={scooter.latitude}
-        onClick={() => handleMarkerClick(scooter)}
-      >
-        {/* <div style={{ cursor: "pointer" }}>📍</div> */}
-      </Marker>
-    ));
-
-  useEffect(() => {
-    console.log("Selected scooter", selectedScooter);
-
-    return () => {};
-  }, [selectedScooter]);
-
   return (
     <div>
       <Map
-        mapStyle="mapbox://styles/mapbox/streets-v10"
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v10"
         initialViewState={{
           longitude: 32.866287,
           latitude: 39.925533,
-          zoom: 12,
+          zoom: 14,
         }}
         style={{
           height: "100vh",
           width: "100wh",
         }}
       >
-        {pins}
-        {/* {popups} */}
+        {scooters.map((scooter) => (
+          <Marker
+            key={scooter.id}
+            longitude={scooter.longitude}
+            latitude={scooter.latitude}
+            onClick={() => handleMarkerClick(scooter)}
+          >
+            {/* <div style={{ cursor: "pointer" }}>📍</div> */}
+          </Marker>
+        ))}
 
         {selectedScooter && (
-          <div>
-            <Popup
-              longitude={selectedScooter.longitude}
-              latitude={selectedScooter.latitude}
-              anchor="bottom"
-              offset={{
-                bottom: [0, -40],
-              }}
-              onOpen={() => {
-                console.log("Rendering popup for scooter:", selectedScooter);
-              }}
-              onClose={() => {
-                console.log("Popup closed");
-                setSelectedScooter(null);
-              }}
-              //   closeOnClick={true}
-            >
-              <div>
-                <h3>Scooter ID: {selectedScooter.id}</h3>
-                <p>Battery: {selectedScooter.battery}%</p>
-              </div>
-            </Popup>
-          </div>
+          <Popup
+            longitude={selectedScooter.longitude}
+            latitude={selectedScooter.latitude}
+            anchor="bottom"
+            offset={{
+              bottom: [0, -40],
+            }}
+            onOpen={() => {
+              console.log("Rendering popup for scooter:", selectedScooter);
+            }}
+            onClose={() => {
+              console.log("Popup closed");
+              setSelectedScooter(null);
+            }}
+            closeOnClick={false}
+          >
+            <div>
+              <h3>Scooter ID: {selectedScooter.id}</h3>
+              <p>Battery: {selectedScooter.battery}%</p>
+            </div>
+          </Popup>
         )}
+        <Popup longitude={-100} latitude={40} anchor="bottom">
+          You are here
+        </Popup>
       </Map>
 
       <div style={{ position: "absolute", top: "10px", right: "10px" }}>
